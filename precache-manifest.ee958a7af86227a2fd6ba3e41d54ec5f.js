5443ce97977bb16c38b42ee7bcda7b78af4aab93
Victor Ajibade <adesojimav@gmail.com>
2024-09-24 02:27:27 +0100

    Updates

---
+++ precache-manifest.ee958a7af86227a2fd6ba3e41d54ec5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4c64189865bcaaa0cab346c116545f7",
+    "url": "/sojimav/JeanEdwardsReact.git/index.html"
+  },
+  {
+    "revision": "ed8e0e743413b1496574",
+    "url": "/sojimav/JeanEdwardsReact.git/static/css/main.0c7a2c2b.chunk.css"
+  },
+  {
+    "revision": "ea7c53ec792092b04f63",
+    "url": "/sojimav/JeanEdwardsReact.git/static/js/2.b9ae8d48.chunk.js"
+  },
+  {
+    "revision": "afae8725e7d5172c6ee86f29e0116382",
+    "url": "/sojimav/JeanEdwardsReact.git/static/js/2.b9ae8d48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "263626f49619460d832b",
+    "url": "/sojimav/JeanEdwardsReact.git/static/js/3.ea305fe1.chunk.js"
+  },
+  {
+    "revision": "ed8e0e743413b1496574",
+    "url": "/sojimav/JeanEdwardsReact.git/static/js/main.74b8d36c.chunk.js"
+  },
+  {
+    "revision": "070f36dfe631f575e256",
+    "url": "/sojimav/JeanEdwardsReact.git/static/js/runtime-main.d821b4de.js"
+  }
+]);
